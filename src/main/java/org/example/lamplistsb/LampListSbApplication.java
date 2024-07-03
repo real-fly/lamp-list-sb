68daf0a4d791aef9d800deadaa7849641f98e75a
fly <2197468432@qq.com>
2024-07-03 14:47:03 +0800

    init

---
+++ src/main/java/org/example/lamplistsb/LampListSbApplication.java
@@ -0,0 +1,13 @@
+package org.example.lamplistsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LampListSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LampListSbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/lamplistsb/LampListSbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lamplistsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LampListSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
